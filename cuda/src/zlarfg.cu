9c2ff6d93c91cce809e4eb6d0d3f020f044ee704
zhe.zhang <18890092219@163.com>
2025-12-31 11:02:57 +0800

    📃 docs(cuda): zlarfg add comments
    zlarfg功能为：计算householder矩阵，H**H * x = beta * e1

--- cuda/src/zlarfg.cu
+++ cuda/src/zlarfg.cu
@@ -1,6 +1,28 @@
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
 
+/**
+*  ZLARFG generates a complex elementary reflector H of order n, such
+*  that
+* 
+*        H**H * ( alpha ) = ( beta ),   H**H * H = I.
+*               (   x   )   (   0  )
+* 
+*  where alpha and beta are scalars, with beta real, and x is an
+*  (n-1)-element complex vector. H is represented in the form
+* 
+*        H = I - tau * ( 1 ) * ( 1 v**H ) ,
+*                      ( v )
+* 
+*  where tau is a complex scalar and v is a complex (n-1)-element
+*  vector. Note that H is not hermitian.
+* 
+*  If the elements of x are all zero and alpha is real, then tau = 0
+*  and H is taken to be the unit matrix.
+* 
+*  Otherwise  1 <= real(tau) <= 2  and  abs(tau-1) <= 1 .
+ */
+
 void zlarfg_gpu(
     cublasHandle_t handle,
     int n,
